cde2759529bd4977215850af1ae53ba01963365a
Alex-Mangust <sanya.stogny@gmail.com>
2026-09-04 02:14:33 +0800

    Работа со слайдером завершена

---
+++ scripts/script.js
@@ -0,0 +1,113 @@
+window.addEventListener("load", function () {
+    const slider = document.querySelector(".slider__inside");
+    let slides = Array.from(slider.children);
+
+    const indicators = document.querySelector(".indicators");
+    slides.forEach(() => {
+        const indicator = document.createElement("div");
+        indicator.classList.add("indicators__item");
+        indicators.appendChild(indicator);
+    });
+    Array.from(indicators.children)[0].classList.add("indicators__item_active");
+
+
+
+    const firstClone = slides[0].cloneNode(true);
+    const lastClone = slides[slides.length - 1].cloneNode(true);
+    slider.appendChild(firstClone);
+    slider.insertBefore(lastClone, slides[0]);
+
+    slides = Array.from(slider.children);
+
+
+    let indexSlayde = 1;
+    const widthSlayde = slider.parentElement.offsetWidth;
+    let shiftSlayder = -widthSlayde;
+    shift();
+
+    let timer;
+    function timerNextSlayde() {
+        timer = setInterval(() => {
+            setIndexSlayde(indexSlayde + 1);
+            if (indexSlayde < slides.length) {
+                shiftSlayder -= widthSlayde;
+                shift();
+            } else {
+                slider.style.transform = `translateX(0px)`;
+                shiftSlayder = 0;
+                setIndexSlayde(0);
+            }
+        }, 10000)
+    };
+
+    let nextSlayde = true;
+
+    const sliderButtons = document.querySelectorAll(".button-slider");
+    sliderButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (nextSlayde) {
+                nextSlayde = false;
+                clearInterval(timer);
+                if (button.classList.contains("right")) {
+                    shiftSlayder -= widthSlayde;``
+                    setIndexSlayde(indexSlayde + 1);
+                } else {
+                    shiftSlayder += widthSlayde;
+                    setIndexSlayde(indexSlayde - 1);
+                };
+                shift();
+                timerNextSlayde(timer);
+                button.classList.add("button-slider_no-active");
+                setTimeout(() => {
+                    button.classList.remove("button-slider_no-active");
+                }, 500);
+            }
+        });
+    });
+
+    timerNextSlayde(timer);
+    function shift() {
+        slider.style.transform = `translateX(${shiftSlayder}px)`;
+        let animation = true;
+        if (indexSlayde == slides.length - 1) {
+            setIndexSlayde(1);
+            shiftSlayder = -widthSlayde;
+            animation = false;
+        } else if (indexSlayde == 0) {
+            setIndexSlayde(slides.length - 2);
+            shiftSlayder -= widthSlayde * 6;
+            animation = false;
+        }
+        setTransition(animation);
+    }
+
+    function setTransition(enable) {
+        if (enable) {
+            slider.style.transition = "transform 0.5s ease";
+        }
+        slider.addEventListener("transitionend", function transitionEnd(event) {
+            if (event.propertyName != "transform") {
+                return;
+            }
+            nextSlayde = true;
+
+            slider.removeEventListener("transitionend", transitionEnd);
+
+            if (!enable) {
+                slider.style.transition = "none";
+                slider.style.transform = `translateX(${shiftSlayder}px)`;
+            }
+        });
+    }
+
+    function setIndexSlayde(newData) {
+        if (indexSlayde < slides.length - 1) {
+            Array.from(indicators.children)[indexSlayde - 1].classList.remove("indicators__item_active");
+        }
+        indexSlayde = newData;
+        if (indexSlayde < slides.length - 1) {
+            Array.from(indicators.children)[indexSlayde - 1].classList.add("indicators__item_active");
+
+        }
+    }
+});
